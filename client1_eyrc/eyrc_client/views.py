482252386807d8f099462adb17c31bc951daa00d
eyic1eyantra <eyic1@e-yantra>
2020-06-19 00:38:36 +0530

    Added client

--- client1_eyrc/eyrc_client/views.py
+++ client1_eyrc/eyrc_client/views.py
@@ -7,6 +7,10 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import render,redirect
 from authlib.integrations.django_client import OAuth
+
+from authlib.oidc.core import CodeIDToken
+from authlib.jose import jwt
+
 oauth = OAuth()
 
 oauth.register(
@@ -19,16 +23,29 @@ oauth.register(
     authorize_url = "https://127.0.0.1:8000/oauth/authorize",
     redirect_url = "https://127.0.0.2/authorize",
     grant_type = "authorization_code",
+    id_token_signing_alg_values_supported = ['RS256'],
     client_kwargs = {
         'scope':'openid profile',
         'token_endpoint_auth_method': 'client_secret_basic',
+    },
+    jwks = {
+    "keys":   [
+                {
+                "kty": "RSA",
+                "e": "AQAB",
+                "use": "sig",
+                # "kid": "b16de1b2ab0c16ac0acf662ef01f7567e544252b",
+                "alg": "RS256",
+                "n": "Lub5dSqbmYujN0L8xhftuV66YeZiai4HoVTG_fjdKEWv7Mpsn3AOIaw0RCs55OJI1PlnBs2ujttvuFQlt8kzNJFpyOOTkbuPrWwxjNlHy44tR42iqiIrt7DUgC5NDseMXl-4t8Psa6QP2NGngvSHu5AiRoTs3UEEVPbtO_Hf6SqtsZJBjUpUUjncUwbMeT2Rp6bff1WblWnNql2lzSJNXldaO9jx4ROcWwHzuSocFvPzwRxz2QRQXhPwKO-DakdXPR4eyhkk7MwrT0ifQjAF09m_3gprWTmtAw9ruMBYkZgyN5IgCkSEME4lY-tdypsdzssmE5rPlXm2TIisJxLPtQs6pv-X_y54ubSUzQaaEY0yMt_EaX2YNIKi-0c5RqWZOWcHvoigOgTgj72M22ffO_-BNOf0ak4qjeDb-fw4OnwApJ3QdsDr2MmVsJFym2rv1gHkbBIQkX51t8zZXYV42-emW3EajqfGe3rC6j48LM7QJzSuStlsMFnyvjU7v1HpwR_BdLg2oLHC9aKBsOuwJCagJIDCX4lFPGob-_uxrDShhBT79GHDM0DGag4at4oJ2oUeQfGvavRcO3Xalc06ECP91dDYF14JjQex3c1z0NEvqSGTXVFgT94YPeXEEjIH5FsXsf7472Pzw9h84ArLCPYGhUhi0Rxrvus"
+                }
+            ]
     },
     prompt = "select_account"
 )
 
 
 def home(request):
-    user = request.session.get('current_user')
+    user = request.session.get('user')
     print('client user: session',user)
     if user:
         user = json.dumps(user)
@@ -47,11 +64,10 @@ def auth(request):
     eyantra = oauth.create_client('eyantra')
     token = eyantra.authorize_access_token(request,verify=False,redirect_uri="https://127.0.0.2:8001/authorize",grant_type="authorization_code")
     print('token is :',token)
-    print(type(token))
     user = eyantra.parse_id_token(request, token)
     print('success________________________________________________')
     print(user)
-    request.session['user'] = user
+    request.session['user'] = user.get('name')
     return redirect('/')
 
 def logout(request):
